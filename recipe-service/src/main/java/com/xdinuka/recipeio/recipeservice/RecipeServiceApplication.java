140675421b23d68e04cb30f40cd6da82eae41cd6
Dinuka Karunananda <dinuka.nirosh@gmail.com>
2020-03-08 13:57:57 +0530

    Updated services to work with discovary service

--- recipe-service/src/main/java/com/xdinuka/recipeio/recipeservice/RecipeServiceApplication.java
+++ recipe-service/src/main/java/com/xdinuka/recipeio/recipeservice/RecipeServiceApplication.java
@@ -2,8 +2,10 @@ package com.xdinuka.recipeio.recipeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RecipeServiceApplication {
 
 	public static void main(String[] args) {
