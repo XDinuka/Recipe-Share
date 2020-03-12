672cc451011d752227f603703d860c8e19bc694a
Dinuka Karunananda <dinuka.nirosh@gmail.com>
2020-03-13 02:28:49 +0530

    Created main navigation component

--- Angular/RecipeIO/src/app/app.module.ts
+++ Angular/RecipeIO/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RioNavbarComponent } from './rio-navbar/rio-navbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { RioNavbarComponent } from './rio-navbar/rio-navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
